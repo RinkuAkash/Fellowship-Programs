70e25b273f7e79335826c76b15cef4ebc52e36cf
RinkuAkash <vinaykmr149@gmail.com>
2020-01-02 10:01:16 +0530

    [Akash] refactor. updated coupon number program. Added N distinct number to max limit of random function

--- Programming/LogicalPrograms/CouponNumbers.py
+++ Programming/LogicalPrograms/CouponNumbers.py
@@ -1,12 +1,11 @@
 import random
 N=int(input())
-max_limit=int(10**(N))
-min_limit=int(10**(N-1))
 distinct_set=set()
 count=0
 while(len(distinct_set)!=N):
-    New_no=random.randint(min_limit,max_limit)
+    New_no=random.randint(1,N)
     if New_no not in distinct_set:
         distinct_set.add(New_no)
     count+=1
+print(distinct_set)
 print("Total random numbers need to have all distinct numbers are: %d"%count)

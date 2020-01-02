19abcc296effc8f38b507c16c9883e1326526eed
RinkuAkash <vinaykmr149@gmail.com>
2020-01-02 15:40:02 +0530

    [Akash] Add. Added word search using binary search

---
+++ Programming/AlgorithmPrograms/WordSearch.py
@@ -0,0 +1,24 @@
+f=open("1-1000.txt","r")
+f1=f.readlines()
+ar=[]
+for i in f1:
+    ar.append(i.rstrip('\n'))
+ar.sort()
+
+word=str(input())
+low=0
+high=len(ar)-1
+flag=False
+while(low<high):
+    mid=(low+high)//2
+    if ar[mid]==word:
+        flag=True
+        break
+    if ar[mid]>word:
+        high=mid-1
+    else:
+        low=mid+1
+if flag==True:
+    print("Found")
+else:
+    print("Not Found")
